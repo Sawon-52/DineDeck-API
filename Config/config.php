327d541a18179087c49ace6906f80e213d9dab3e
Sawon-52 <mehedihasansawon.52@gmail.com>
2025-12-16 00:08:51 +0600

    project file setup

--- Config/config.php
+++ Config/config.php
@@ -1,6 +1,14 @@
 <?php
+
 return [
     "app" => [
         "name" => $_ENV['APP_NAME'],
+        "environment" => $_ENV['APP_ENV'],
+    ],
+    "database" => [
+        "host" => $_ENV['DB_HOST'] ?? 'localhost',
+        "database" => $_ENV['DB_NAME'] ?? '',
+        "username" => $_ENV['DB_USER'] ?? 'root',
+        "password" => $_ENV['DB_PASS'] ?? '', 
     ]
     ];
--- public/index.php
+++ public/index.php
@@ -1,15 +1,20 @@
 <?php
 require __DIR__ .'/../vendor/autoload.php';
-
 use Dotenv\Dotenv;
-
+use core\Router\Router;
 // Here Findout .env file and load env variable 
 $dotenv = Dotenv::createImmutable(__DIR__.'/../');
 $dotenv->load();
 
-$config = require __DIR__ .'/../Config/config.php';
-echo "<pre>";
-print_r( $config['app']['name']);
+// Init router
+$router = new Router();
+
+// Load routes
+require __DIR__ . '/../routes/api.php';
+
+// Dispatch request
+$router->dispatch();
+
 
 
 
